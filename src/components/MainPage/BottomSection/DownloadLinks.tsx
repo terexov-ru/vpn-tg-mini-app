572599402120653b96411aaab4a2223b333b52f9
Karina Kabanova <karina.kabanova@jove.com>
2025-02-07 14:26:15 -0300

    r

--- src/components/MainPage/BottomSection/DownloadLinks.tsx
+++ src/components/MainPage/BottomSection/DownloadLinks.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { div } from "framer-motion/client";
+
 const apps = [
   { name: "iOS", link: "/download/ios", icon: "🍏" },
   { name: "Android", link: "/download/android", icon: "🤖" },
@@ -9,20 +11,20 @@ const apps = [
 
 export default function DownloadLinks() {
   return (
-    <div className="overflow-x-auto no-scrollbar w-full">
-      <div className="flex gap-4 min-w-max">
-        {apps.map((app) => (
-          <a
-            key={app.name}
-            href="#!"
-            className={
-              "flex flex-col items-start justify-between w-32 h-28 rounded-xl p-4 font-semibold transition bg-gray-900 text-gray-400"
-            }
-          >
-            <span className="text-3xl">{app.icon}</span>
-            <span className="text-sm">{app.name}</span>
-          </a>
-        ))}
+    <div className="px-4 max-w-full overflow-x-auto">
+      <div className="overflow-x-auto w-full">
+        <div className="flex gap-4 min-w-max overflow-x-auto no-scrollbar">
+          {apps.map((app) => (
+            <a
+              key={app.name}
+              href={app.link}
+              className="flex flex-col items-start justify-between w-32 h-28 rounded-xl p-4 font-semibold transition bg-gray-900 text-gray-400"
+            >
+              <span className="text-3xl">{app.icon}</span>
+              <span className="text-sm">{app.name}</span>
+            </a>
+          ))}
+        </div>
       </div>
     </div>
   );
