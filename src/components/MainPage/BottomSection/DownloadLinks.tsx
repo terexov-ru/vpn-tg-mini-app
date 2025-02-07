5e90c708cd74935fd1f32a6d85ee299b999f8c20
Karina Kabanova <karina.kabanova@jove.com>
2025-02-07 13:53:17 -0300

    test

--- src/components/MainPage/BottomSection/DownloadLinks.tsx
+++ src/components/MainPage/BottomSection/DownloadLinks.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { useEffect, useState } from "react";
-
 const apps = [
   { name: "iOS", link: "/download/ios", icon: "🍏" },
   { name: "Android", link: "/download/android", icon: "🤖" },
@@ -10,21 +8,6 @@ const apps = [
 ];
 
 export default function DownloadLinks() {
-  const [platform, setPlatform] = useState("ios");
-
-  useEffect(() => {
-    const userAgent = navigator.userAgent.toLowerCase();
-    if (userAgent.includes("android")) {
-      setPlatform("Android");
-    } else if (userAgent.includes("iphone") || userAgent.includes("ipad")) {
-      setPlatform("iOS");
-    } else if (userAgent.includes("win")) {
-      setPlatform("Windows");
-    } else if (userAgent.includes("mac")) {
-      setPlatform("macOS");
-    }
-  }, []);
-
   return (
     <div className="overflow-x-auto no-scrollbar w-full">
       <div className="flex gap-4 min-w-max">
@@ -32,11 +15,9 @@ export default function DownloadLinks() {
           <a
             key={app.name}
             href="#!"
-            className={`flex flex-col items-start justify-between w-32 h-28 rounded-xl p-4 font-semibold transition ${
-              platform === app.name
-                ? "bg-[#6F40DC] text-white"
-                : "bg-gray-900 text-gray-400"
-            }`}
+            className={
+              "flex flex-col items-start justify-between w-32 h-28 rounded-xl p-4 font-semibold transition bg-gray-900 text-gray-400"
+            }
           >
             <span className="text-3xl">{app.icon}</span>
             <span className="text-sm">{app.name}</span>
