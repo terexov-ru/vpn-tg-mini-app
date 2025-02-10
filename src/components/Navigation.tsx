e250015e87b32e7ddc46b26f5af27f5ed8b7dac6
Karina Kabanova <karina.kabanova@jove.com>
2025-02-10 17:06:36 -0300

    fix nav

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Home, ShieldCheck, Settings } from "lucide-react";
+import { motion } from "framer-motion";
 
 export default function Navigation() {
   const pathname = usePathname();
@@ -19,29 +20,38 @@ export default function Navigation() {
         const isActive = pathname === href;
 
         return (
-          <Link key={href} href={href} className="relative">
-            <div
-              className={`flex items-center justify-center rounded-full bg-[#111] transition-all duration-300 ${
-                isActive
-                  ? "px-3 py-1 text-white shadow-lg w-auto"
-                  : "w-12 h-12 "
-              }`}
+          <Link
+            key={href}
+            href={href}
+            className="flex items-center justify-center rounded-full bg-[#111] shadow-lg text-white h-[50px]"
+          >
+            <motion.div
+              className="inline-flex items-center overflow-hidden px-2 py-1 h-full"
+              animate={{
+                maxWidth: isActive ? 130 : 50,
+              }}
+              transition={{ duration: 0.3, ease: "easeInOut" }}
             >
               <div
-                className={`w-10 h-10 flex items-center justify-center rounded-full transition-all duration-300 ${
+                className={`w-8 h-8 min-w-8 min-h-8 flex items-center justify-center rounded-full ${
                   isActive ? "bg-[#6F40DC] shadow-lg" : "bg-transparent"
                 }`}
               >
-                <Icon className="w-6 h-6 text-white" />
+                <Icon className="w-5 h-5" />
               </div>
-              <span
-                className={`text-sm font-normal ml-2 transition-all duration-300 ${
-                  isActive ? "opacity-100 w-auto" : "opacity-0 w-0 hidden"
-                }`}
+
+              <motion.span
+                className="text-xs font-normal whitespace-nowrap"
+                animate={{
+                  opacity: isActive ? 1 : 0,
+                  width: isActive ? "auto" : 0,
+                  marginLeft: isActive ? "8px" : 0,
+                }}
+                transition={{ duration: 0.3, delay: isActive ? 0.1 : 0 }}
               >
                 {label}
-              </span>
-            </div>
+              </motion.span>
+            </motion.div>
           </Link>
         );
       })}
