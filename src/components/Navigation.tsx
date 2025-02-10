7ea87f4aa494f25d6e1bfae7dbc627a693f5e3d3
Karina Kabanova <karina.kabanova@jove.com>
2025-02-10 17:53:50 -0300

    fix

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,9 +4,15 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Home, ShieldCheck, Settings } from "lucide-react";
 import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
 
 export default function Navigation() {
   const pathname = usePathname();
+  const [isFirstLoad, setIsFirstLoad] = useState(true);
+
+  useEffect(() => {
+    setIsFirstLoad(false);
+  }, []);
 
   const menuItems = [
     { href: "/", icon: Home, label: "Главная" },
@@ -15,7 +21,7 @@ export default function Navigation() {
   ];
 
   return (
-    <nav className="flex justify-center items-center gap-4 p-3 fixed bottom-0 w-full max-w-[500px] mx-auto">
+    <nav className="flex justify-center items-center gap-4 p-3 fixed bottom-0 w-full max-w-[500px] mx-auto bg-black">
       {menuItems.map(({ href, icon: Icon, label }) => {
         const isActive = pathname === href;
 
@@ -27,10 +33,15 @@ export default function Navigation() {
           >
             <motion.div
               className="inline-flex items-center overflow-hidden px-2 py-1 h-full"
+              initial={isFirstLoad ? { maxWidth: isActive ? 130 : 50 } : {}}
               animate={{
                 maxWidth: isActive ? 130 : 50,
               }}
-              transition={{ duration: 0.3, ease: "easeInOut" }}
+              transition={
+                isFirstLoad
+                  ? { duration: 0 }
+                  : { duration: 0.3, ease: "easeInOut" }
+              }
             >
               <div
                 className={`w-8 h-8 min-w-8 min-h-8 flex items-center justify-center rounded-full ${
@@ -42,12 +53,25 @@ export default function Navigation() {
 
               <motion.span
                 className="text-xs font-normal whitespace-nowrap"
+                initial={
+                  isFirstLoad
+                    ? {
+                        opacity: isActive ? 1 : 0,
+                        width: isActive ? "auto" : 0,
+                        marginLeft: isActive ? "8px" : 0,
+                      }
+                    : {}
+                }
                 animate={{
                   opacity: isActive ? 1 : 0,
                   width: isActive ? "auto" : 0,
                   marginLeft: isActive ? "8px" : 0,
                 }}
-                transition={{ duration: 0.3, delay: isActive ? 0.1 : 0 }}
+                transition={
+                  isFirstLoad
+                    ? { duration: 0 }
+                    : { duration: 0.3, delay: isActive ? 0.1 : 0 }
+                }
               >
                 {label}
               </motion.span>
