7d5d832e707f6d289963b6977485084ba7fc2927
Karina Kabanova <karina.kabanova@jove.com>
2025-02-10 17:58:30 -0300

    ficc

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
       </div>
 
       <div className="z-10">
-        <div className="overflow-y-auto max-h-[calc(100vh-74px)]">
+        <div className="overflow-y-auto max-h-[calc(100dvh-74px)]">
           {children}
         </div>
         <Navgation />
